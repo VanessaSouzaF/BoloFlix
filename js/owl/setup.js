01ec4ad70cd67343d26fda14add0032e773a96d9
Vanna <vanessacat.souza@gmail.com>
2022-04-20 23:22:32 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+$('.owl-carousel').owlCarousel({
+    items:1,
+    margin:10,
+    autoHeight:true
+});
